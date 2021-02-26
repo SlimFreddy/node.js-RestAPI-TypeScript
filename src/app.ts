ed3514bd09e462db0e5e4f577d1d26d0f0f15d6e
Freddy <Frederik.Nies@btc-ag.com>
2021-02-26 13:16:37 +0100

    change response json by error

--- src/app.ts
+++ src/app.ts
@@ -10,27 +10,34 @@ import userController from "./controller/UserController";
 // ENVIRONMENT  FILE
 dotenv.config();
 // DATABASE CONNECTION
-mongoose.connect(process.env.MONGO_URL as string, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-}, ()=>{
-  console.log('\x1b[33m%s\x1b[0m',`[database]: Server connect to ${process.env.MONGO_URL} database`)
-});
+mongoose.connect(
+  process.env.MONGO_URL as string,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  () => {
+    console.log(
+      "\x1b[33m%s\x1b[0m",
+      `[database]: Server connect to ${process.env.MONGO_URL} database`
+    );
+  }
+);
 const app = express();
 // SERVER LOGGER FOR REQUEST
 app.use(morgan("dev"));
 // JSON FORMATTER
 app.use(express.json());
 //CORS SETTINGS
-app.use((req, res, next)=>{
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', '*');
-  if(req.method === 'OPTIONS'){
-    res.header('Access-Control-Allow-METHOD', 'POST, GET, DELETE');
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "*");
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-METHOD", "POST, GET, DELETE");
     return res.status(200).json({});
   }
   next();
-})
+});
 //AUTHENTICATION REST API
 app.use("/api/auth", authController);
 //POST REST API
@@ -47,16 +54,15 @@ app.use(
   (err: HttpException, req: Request, res: Response, next: NextFunction) => {
     res.status(err.status || 500);
     res.json({
-      error: {
-        status: err.status || 500,
-        message: err.message,
-      },
+      status: err.status || 500,
+      message: err.message,
     });
   }
 );
 // SERVER PORT
 app.listen(process.env.SERVER_PORT, () => {
   console.log(
-    '\x1b[33m%s\x1b[0m',  `[server]: Server is running at https://localhost:${process.env.SERVER_PORT}`
+    "\x1b[33m%s\x1b[0m",
+    `[server]: Server is running at https://localhost:${process.env.SERVER_PORT}`
   );
 });
