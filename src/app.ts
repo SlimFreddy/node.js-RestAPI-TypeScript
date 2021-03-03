9d9d41ef97c1711fd2a36d0c209141892cf7aff5
Freddy <Frederik.Nies@btc-ag.com>
2021-03-03 10:58:39 +0100

    fix cors for delete rquest

--- src/app.ts
+++ src/app.ts
@@ -33,7 +33,7 @@ app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "*");
   if (req.method === "OPTIONS") {
-    res.header("Access-Control-Allow-METHOD", "POST, GET, DELETE");
+    res.header('Access-Control-Allow-Methods', 'DELETE, POST, GET');
     return res.status(200).json({});
   }
   next();
